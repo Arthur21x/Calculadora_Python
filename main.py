641d5a8e71142b6531dadb23e1b247fe949902df
Arthur21x <86072531+Arthur21x@users.noreply.github.com>
2023-12-05 09:01:04 -0300

    Atualização do Projeto

--- main.py
+++ main.py
@@ -1,84 +1,38 @@
+from PySide6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel, QLineEdit, QTextEdit
+from display import Display
+from PySide6.QtGui import QIcon
+from variables import *
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout
-from PyQt5.QtWidgets import QPushButton, QLineEdit, QSizePolicy
 
 
 class Calculadora(QMainWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.setWindowTitle('Calculadora')
-        self.setFixedSize(480, 480)
+
         self.cw = QWidget()
-        self.grid = QGridLayout(self.cw)
-
-        self.display = QLineEdit()
-        self.grid.addWidget(self.display, 0, 0, 1, 5)
-        self.display.setDisabled(True)
-        self.setStyleSheet(
-            '* {background: #FFF; color: #000; font-size: 30px;}'
-        )
-        self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
-        self.add_btn(QPushButton('7'), 1, 0, 1, 1)
-        self.add_btn(QPushButton('8'), 1, 1, 1, 1)
-        self.add_btn(QPushButton('9'), 1, 2, 1, 1)
-        self.add_btn(QPushButton('+'), 1, 3, 1, 1)
-        self.add_btn(QPushButton('C'), 1, 4, 1, 1,
-                     lambda: self.display.setText(''),
-                     'background: #d5580d; color: #fff; font-wieght: 700;')
-
-        self.add_btn(QPushButton('4'), 2, 0, 1, 1)
-        self.add_btn(QPushButton('5'), 2, 1, 1, 1)
-        self.add_btn(QPushButton('6'), 2, 2, 1, 1)
-        self.add_btn(QPushButton('-'), 2, 3, 1, 1)
-        self.add_btn(QPushButton('<-'), 2, 4, 1, 1,
-                     lambda: self.display.setText(
-                         self.display.text()[:-1]
-                     ), 'background: #13823a; color #fff, font-weight:700;')
-
-        self.add_btn(QPushButton('1'), 3, 0, 1, 1)
-        self.add_btn(QPushButton('2'), 3, 1, 1, 1)
-        self.add_btn(QPushButton('3'), 3, 2, 1, 1)
-        self.add_btn(QPushButton('/'), 3, 3, 1, 1)
-        self.add_btn(QPushButton('π'), 3, 4, 1, 1)
-
-        self.add_btn(QPushButton('.'), 4, 0, 1, 1)
-        self.add_btn(QPushButton('0'), 4, 1, 1, 1)
-        self.add_btn(QPushButton(''), 4, 2, 1, 1)
-        self.add_btn(QPushButton('*'), 4, 3, 1, 1)
-        self.add_btn(QPushButton('='), 4, 4, 1, 1,
-                     self.eval_igual,
-                     'background: #095177; color #fff, font-weight:700;')
+        self.vLayout = QVBoxLayout()
+        self.cw.setLayout(self.vLayout)
 
         self.setCentralWidget(self.cw)
+        self.setWindowTitle('Calculadora')
 
-    def add_btn(self, btn, row, col, rowspan, colspan, funcao=None, style=None):
-        self.grid.addWidget(btn, row, col, rowspan, colspan)
+    def adjustFixedSize(self):
+        self.adjustSize()
+        self.setFixedSize(self.width(), self.height())
 
-        if not funcao:
-            btn.clicked.connect(
-                lambda: self.display.setText(self.display.text() + btn.text())
-            )
-        else:
-            btn.clicked.connect(funcao)
-
-        if style:
-            btn.setStyleSheet(style)
-
-        btn.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
-
-    def eval_igual(self):
-        try:
-            self.display.setText(
-                str(eval(self.display.text()))
-            )
-        except Exception as e:
-            self.display.text('Conta inválida.')
+    def addWidgetToVLayout(self, display):
+        self.vLayout.addWidget(display)
 
 
 if __name__ == '__main__':
-    qt = QApplication(sys.argv)
-    calc = Calculadora()
-    calc.show()
-    qt.exec_()
+    app = QApplication(sys.argv)
+    Window = Calculadora()
+    icon = QIcon(str(WINDOW_ICON_PATH))
+    Window.setWindowIcon(icon)
 
+    display = Display()
 
+    Window.addWidgetToVLayout(display)
+#    Window.adjustFixedSize()
+    Window.show()
+    app.exec()
